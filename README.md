3ccc738d70a49963512bc4281d3e82956bcb8d8f
Bill Katz <billkatz@gmail.com>
2014-04-11 10:05:23 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+dvid-console [![Picture](https://raw.github.com/janelia-flyem/janelia-flyem.github.com/master/images/jfrc_grey_180x40.png)](http://www.janelia.org)
+=============
+
+Web console for [DVID](https://github.com/janelia-flyem/dvid), a distributed, versioned
+image-oriented datastore.  The console allows administration of a DVID server as well
+as an automated API help system using [RAML](http://www.raml.org).  
+
+This code can be used with DVID in two ways:
+* Specify the repo location using the `-webclient=/path/to/repo` option with the DVID `serve`
+command.
+* Compile the entire repository into gzipped data that is included in standard Go files.  This
+allows a single DVID executable to have an embedded console.
---
+++ console/js/spa.js
@@ -0,0 +1,95 @@
+/* Routines necessary to tune single-page web app derived from the original Charisma system. */
+
+Object.size = function(obj) {
+    var size = 0, key;
+    for (key in obj) {
+        if (obj.hasOwnProperty(key)) size++;
+    }
+    return size;
+};
+
+$(document).ready(function(){
+	//disabling some functions for Internet Explorer
+	if($.browser.msie)
+	{
+		$('#toggle-fullscreen').hide();
+		$('.login-box').find('.input-large').removeClass('span10');
+		
+	}
+	
+	//animating menus on hover
+	$('ul.main-menu li:not(.nav-header)').hover(function(){
+		$(this).animate({'margin-left':'+=5'},300);
+	},
+	function(){
+		$(this).animate({'margin-left':'-=5'},300);
+	});
+	
+	//other things to do on document ready, separated for ajax calls
+	docReady();
+});
+		
+function docReady(){
+	//prevent # links from moving to top
+	$('a[href="#"][data-top!=true]').click(function(e){
+		e.preventDefault();
+	});
+	
+	//rich text editor
+	$('.cleditor').cleditor();
+	
+	//datepicker
+	$('.datepicker').datepicker();
+	
+	//notifications
+	$('.noty').click(function(e){
+		e.preventDefault();
+		var options = $.parseJSON($(this).attr('data-noty-options'));
+		noty(options);
+	});
+
+
+	//uniform - styler for checkbox, radio and file input
+	$("input:checkbox, input:radio, input:file").not('[data-no-uniform="true"],#uniform-is-ajax').uniform();
+
+	//chosen - improves select
+	$('[data-rel="chosen"],[rel="chosen"]').chosen();
+
+	//tabs
+	$('#myTab a:first').tab('show');
+	$('#myTab a').click(function (e) {
+	  e.preventDefault();
+	  $(this).tab('show');
+	});
+
+	//makes elements soratble, elements that sort need to have id attribute to save the result
+	$('.sortable').sortable({
+		revert:true,
+		cancel:'.btn,.box-content,.nav-header',
+		update:function(event,ui){
+			//line below gives the ids of elements, you can make ajax call here to save it to the database
+			//console.log($(this).sortable('toArray'));
+		}
+	});
+
+	//slider
+	$('.slider').slider({range:true,values:[10,65]});
+
+	//tooltip
+	$('[rel="tooltip"],[data-rel="tooltip"]').tooltip({"placement":"bottom",delay: { show: 400, hide: 200 }});
+
+	//auto grow textarea
+	$('textarea.autogrow').autogrow();
+
+	//popover
+	$('[rel="popover"],[data-rel="popover"]').popover();
+
+	//iOS / iPhone style toggle switch
+	$('.iphone-toggle').iphoneStyle();
+
+    $('.btn-setting').click(function(e){
+        e.preventDefault();
+        $('#myModal').modal('show');
+    });
+}
+
