cfd5539b6a8142c14522f1528b69488e813b97b8
Jody Clements <jody@neomorphic.co.uk>
2015-06-12 16:49:40 -0400

    Moved server status window into its own component.
    This makes it easier to add other content to the Admin page, without all
    the functions getting muddled together. It also makes it easier to see
    how the timers and settings are used for the status window.

--- js/components/Admin.react.js
+++ js/components/Admin.react.js
@@ -1,115 +1,8 @@
 var React = require('react'),
   Router = require('react-router'),
-  Link   = Router.Link;
+  Link   = Router.Link,
+  ServerStatus = require('./ServerStatus.react');
 
-// Determine the scale for each type of monitoring on y-axis.
-var datasetYScale = {
-  "key bytes read": {
-    scale: 1000000,
-    units: "MB/sec"
-  },
-  "key bytes written": {
-    scale: 1000000,
-    units: "MB/sec"
-  },
-  "value bytes read": {
-    scale: 1000000,
-    units: "MB/sec"
-  },
-  "value bytes written": {
-    scale: 1000000,
-    units: "MB/sec"
-  },
-  "file bytes read": {
-    scale: 1000000,
-    units: "MB/sec"
-  },
-  "file bytes written": {
-    scale: 1000000,
-    units: "MB/sec"
-  },
-  "handlers active": {
-    scale: 1,
-    units: "%"
-  }
-};
-
-var chartOptions = {
-  series: { shadowSize: 1 }, // drawing is faster without shadows
-  yaxis: { min: 0, max: 100 },
-  xaxis: { show: false },
-  legend: {
-    position: 'nw'
-  }
-};
-
-var loadStats = {},
-  loadData    = [],
-  totalPoints = 300,
-  polledCount = 0,
-  loadInterval = 1000;
-
-
-var setLoadData = function() {
-  var setnum = 0;
-  for (var dataset in loadStats) {
-    var yscale = datasetYScale[dataset];
-    var datasetLabel = dataset;
-    if (yscale.units !== null) {
-      datasetLabel += " (" + yscale.units + ")";
-    }
-    loadData[setnum] = {
-      label: datasetLabel,
-      data: []
-    };
-    var filler = totalPoints - loadStats[dataset].length;
-    for (var x = 0; x < totalPoints; ++x) {
-      if (filler > x) {
-        loadData[setnum].data.push([x,0]);
-      } else {
-        var i = x - filler;
-        if (i < 0 || i > loadStats[dataset].length) {
-          console.log("Bad i:", i, x, filler, loadStats[dataset].length);
-        }
-        var y = loadStats[dataset][i] / yscale.scale;
-
-        if (y < 0) {
-          y = 0;
-        }
-        if (y > 100) {
-          y = 100;
-        }
-        loadData[setnum].data.push([x, y]);
-      }
-    }
-    ++setnum;
-  }
-}
-
-var getLoadStats = function () {
-  $.get('/api/load', function(response) {
-    for (var dataset in response) {
-      if (dataset in datasetYScale) {
-        if (!loadStats.hasOwnProperty(dataset) || Object.prototype.toString.call(loadStats[dataset]) !== '[object Array]') {
-          loadStats[dataset] = [];
-        }
-        if (loadStats[dataset].length >= totalPoints) {
-          loadStats[dataset] = loadStats[dataset].slice(1);
-        }
-        loadStats[dataset].push(response[dataset]);
-      }
-    }
-    polledCount++;
-    setLoadData();
-    console.log(polledCount);
-  });
-  setTimeout(getLoadStats, loadInterval);
-}
-
-var updateLoadStats = function() {
-  $.plot($('p'), loadData, chartOptions).draw();
-  setTimeout(updateLoadStats, loadInterval);
-}
 
 var Admin = React.createClass({
   getInitialState: function() {
@@ -119,9 +12,10 @@ var Admin = React.createClass({
 
   // this gets called after the fist time the component is loaded into the page.
   componentDidMount: function () {
+    return;
+  },
 
-    getLoadStats();
-    updateLoadStats();
+  componentWillUnmount: function () {
     return;
   },
 
@@ -129,7 +23,7 @@ var Admin = React.createClass({
     return (
       <div>
         <h1>Admin</h1>
-        <p className="usageChart">Scrolling usage chart here.</p>
+        <ServerStatus/>
       </div>
     );
   }
---
+++ js/components/ServerStatus.react.js
@@ -0,0 +1,138 @@
+var React = require('react'),
+  Router = require('react-router'),
+  Link   = Router.Link,
+  loadStats = {},
+  loadData    = [],
+  totalPoints = 300,
+  loadInterval = 1000,
+  updateTimeout = null,
+  getTimeout = null;
+
+// Determine the scale for each type of monitoring on y-axis.
+var datasetYScale = {
+  "key bytes read": {
+    scale: 1000000,
+    units: "MB/sec"
+  },
+  "key bytes written": {
+    scale: 1000000,
+    units: "MB/sec"
+  },
+  "value bytes read": {
+    scale: 1000000,
+    units: "MB/sec"
+  },
+  "value bytes written": {
+    scale: 1000000,
+    units: "MB/sec"
+  },
+  "file bytes read": {
+    scale: 1000000,
+    units: "MB/sec"
+  },
+  "file bytes written": {
+    scale: 1000000,
+    units: "MB/sec"
+  },
+  "handlers active": {
+    scale: 1,
+    units: "%"
+  }
+};
+
+var chartOptions = {
+  series: { shadowSize: 1 }, // drawing is faster without shadows
+  yaxis: { min: 0, max: 100 },
+  xaxis: { show: false },
+  legend: {
+    position: 'nw'
+  }
+};
+
+
+
+var setLoadData = function() {
+  var setnum = 0;
+  for (var dataset in loadStats) {
+    var yscale = datasetYScale[dataset];
+    var datasetLabel = dataset;
+    if (yscale.units !== null) {
+      datasetLabel += " (" + yscale.units + ")";
+    }
+    loadData[setnum] = {
+      label: datasetLabel,
+      data: []
+    };
+    var filler = totalPoints - loadStats[dataset].length;
+    for (var x = 0; x < totalPoints; ++x) {
+      if (filler > x) {
+        loadData[setnum].data.push([x,0]);
+      } else {
+        var i = x - filler;
+        if (i < 0 || i > loadStats[dataset].length) {
+          console.log("Bad i:", i, x, filler, loadStats[dataset].length);
+        }
+        var y = loadStats[dataset][i] / yscale.scale;
+
+        if (y < 0) {
+          y = 0;
+        }
+        if (y > 100) {
+          y = 100;
+        }
+        loadData[setnum].data.push([x, y]);
+      }
+    }
+    ++setnum;
+  }
+}
+
+var getLoadStats = function () {
+  $.get('/api/load', function(response) {
+    for (var dataset in response) {
+      if (dataset in datasetYScale) {
+        if (!loadStats.hasOwnProperty(dataset) || Object.prototype.toString.call(loadStats[dataset]) !== '[object Array]') {
+          loadStats[dataset] = [];
+        }
+        if (loadStats[dataset].length >= totalPoints) {
+          loadStats[dataset] = loadStats[dataset].slice(1);
+        }
+        loadStats[dataset].push(response[dataset]);
+      }
+    }
+    setLoadData();
+  });
+  setTimeout(getLoadStats, loadInterval);
+}
+
+var updateLoadStats = function() {
+  $.plot($('.usageChart'), loadData, chartOptions).draw();
+  setTimeout(updateLoadStats, loadInterval);
+}
+
+var ServerStatus = React.createClass({
+  getInitialState: function() {
+    return {
+    };
+  },
+
+  // this gets called after the fist time the component is loaded into the page.
+  componentDidMount: function () {
+
+    getLoadStats();
+    updateLoadStats();
+    return;
+  },
+
+  componentWillUnmount: function () {
+    window.clearTimeout(updateTimeout);
+  },
+
+  render: function () {
+    return (
+      <div className="usageChart"></div>
+    );
+  }
+});
+
+module.exports = ServerStatus;
