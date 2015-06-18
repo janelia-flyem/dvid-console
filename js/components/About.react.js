5fee6938f0f40de71a0480433be6edf73aecfb43
Jody Clements <jody@neomorphic.co.uk>
2015-06-18 09:45:51 -0400

    Port of the original dvid welcome page.

--- js/components/About.react.js
+++ js/components/About.react.js
@@ -17,7 +17,26 @@ var About = React.createClass({
   render: function () {
     return (
       <div>
-        <h1>About</h1>
+		    <a id="forkme_banner" href="https://github.com/janelia-flyem/dvid">View DVID on GitHub</a>
+        <h3>Welcome to DVID</h3>
+
+        <p>DVID documentation can be found on the extensive <a href="https://github.com/janelia-flyem/dvid#dvid-">README</a> on
+          github as well as the automatically updated <a href="https://godoc.org/github.com/janelia-flyem/dvid">Go Doc
+          documentation</a>.</p>
+        <p>The <a href="/console/index.html">DVID admin console</a> for this server may be available if you have permissions.</p>
+
+        <h4>HTTP API</h4>
+
+        <p>
+          Each data type within DVID exposes commands and a HTTP API allowing clients like this browser to retrieve and put data.
+          Command-line and HTTP API documentation is currently distributed over a variety of data types.  Visit
+          the <a href="/api/help">/api/help HTTP endpoint</a> to review this server's current API.
+        </p>
+
+        <h3>Licensing</h3>
+        <p>DVID is released under the <a href="http://janelia-flyem.github.com/janelia_farm_license.html">Janelia
+        Farm license</a>, a <a href="http://en.wikipedia.org/wiki/BSD_license#3-clause_license_.28.22New_BSD_License.22_or_.22Modified_BSD_License.22.29"> 3-clause
+        BSD license</a>.</p>
       </div>
     );
   }
