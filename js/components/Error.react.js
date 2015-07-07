e035d7756b8e2f8ac7213f18b954267b7fc94e64
Jody Clements <jody@neomorphic.co.uk>
2015-07-07 16:31:11 -0400

    Added react-bootstrap library
    This makes it easier to include bootstrap components in the app.

--- js/components/Error.react.js
+++ js/components/Error.react.js
@@ -1,19 +1,30 @@
 import React from 'react';
+import {Alert} from 'react-bootstrap';
 
 class Error extends React.Component {
   constructor() {
     super();
-    this.state = {};
-  }
-
-  componentDidMount() {
-    return;
+    this.state = {"visible": true};
   }
 
   render() {
+    if (this.state.visible) {
+      return (
+        <Alert bsStyle='danger' onDismiss={this.handleDismiss.bind(this)}>
+          <h4>Error Message</h4>
+          <p>More information about the error</p>
+        </Alert>
+      );
+    }
+
     return (
-      <div className="alert alert-danger" role="alert">...</div>
+      <div></div>
     );
+
+  }
+
+  handleDismiss() {
+    this.setState({"visible": false});
   }
 };
 
