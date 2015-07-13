ed2a3d9782664c363a7ce004015eadb8b2e5f686
Jody Clements <jody@neomorphic.co.uk>
2015-07-13 09:21:30 -0400

    Truncate log entries longer than 100 characters.
    This stops a single huge log entry from overwhelming the log entry field
    in the repo page.

--- js/components/Repo.react.js
+++ js/components/Repo.react.js
@@ -10,10 +10,15 @@ import RepoDAG from './RepoDAG.react.js';
 class LogEntry extends React.Component {
   render(){
     var entry = this.props.entry.split(/\s(.+)/);
+    var log_text = entry[1];
+    if (log_text.length > 100) {
+      log_text = log_text.substring(0,100) + '...';
+    }
+
     return (
       <tr>
         <td className="timestamp">{moment(entry[0]).format("MMM Do YYYY, h:mm:ss a")}</td>
-        <td>{entry[1]}</td>
+        <td>{log_text}</td>
       </tr>
     );
   }
