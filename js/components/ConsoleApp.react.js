b16d08638c064684c1a15bd969ddef33701fce8f
Jody Clements <jody@neomorphic.co.uk>
2015-07-13 16:17:01 -0400

    Adds TileMap stub.
    This will be filled out to provide the code for rendering the tile maps.

--- js/components/ConsoleApp.react.js
+++ js/components/ConsoleApp.react.js
@@ -13,9 +13,10 @@ var React       = require('react'),
   Error         = require('./Error.react');
 
 import Repo from './Repo.react';
+import TileMap from './TileMap.react';
 
-var ConsoleApp = React.createClass({
-  render: function () {
+class ConsoleApp extends React.Component {
+  render() {
     return (
       <div>
         <Nav />
@@ -27,7 +28,7 @@ var ConsoleApp = React.createClass({
       </div>
     );
   }
-});
+}
 
 var routes = (
   <Route name="consoleapp" path="/" handler={ConsoleApp}>
@@ -35,6 +36,8 @@ var routes = (
     <Route name="admin" path="admin" handler={Admin}/>
     <Route name="about" path="about" handler={About}/>
     <Route name="repo"  path="repo/:uuid"  handler={Repo}/>
+    <Route name="tilemap" path="/repo/:uuid/ts/:tileSource/ls/:labelSource" handler={TileMap}/>
+    <Route name="tilemapwithcoords" path="/repo/:uuid/ts/:tileSource/ls/:labelSource/:plane/:coordinates" handler={TileMap}/>
     <NotFoundRoute handler={NotFound}/>
   </Route>
 );
