d6b00981a435a81ef6da890398f23531830eb749
Rob Svirskas <svirskasr@janelia.hhmi.org>
2015-07-15 16:14:48 -0400

    Node mouseover
    Mouseover on node logs DAG node log to console

--- js/components/RepoDAG.react.js
+++ js/components/RepoDAG.react.js
@@ -7,39 +7,52 @@ import dagreD3 from 'dagre-d3';
 class RepoDAG extends React.Component {
   // Dagre graph
   componentDidMount() {
-    var g = new dagreD3.graphlib.Graph()
-      .setGraph({})
-      .setDefaultEdgeLabel(function() { return {}; });
+    var g = new dagreD3.graphlib.Graph().setGraph({});
     $.each(this.props.repo.DAG.Nodes,function (name,n) {
       var version = n.VersionID;
-      var nc = "";
+      var log = '';
+      if (n.Log.length)
+        log = (n.Log);
+      var nodeclass = "";
       if (n.Locked)
-        nc = "type-locked";
-      g.setNode(version,{ label: version + ': ' + name.substr(0,5), class: nc});
+        nodeclass = "type-locked";
+      g.setNode(version,{ label: version + ': ' + name.substr(0,5),
+                          class: nodeclass,
+                          rx: 5, ry: 5,
+                          log: log,
+                          fullname: version + ': ' + name});
       $.each(n.Children,function (c) {
-        g.setEdge(version,n.Children[c]);
+        g.setEdge(version,n.Children[c],{ lineInterpolate: 'basis', arrowheadStyle: "fill: #000"});
       });
-    });
-    g.nodes().forEach(function(v) {
-      var node = g.node(v);
-      node.rx = node.ry = 5;
-    });
-    g.edges().forEach(function(v) {
-      g.setEdge(v,{lineInterpolate: 'basis',
-                   arrowheadStyle: "fill: #000"});
     });
     var render = new dagreD3.render();
     var svg = d3.select("svg"),
-    svgGroup = svg.append("g");
-    render(d3.select("svg g"), g);
-    var xCenterOffset = (svg.attr("width") - g.graph().width) / 2;
-    svgGroup.attr("transform", "translate(" + xCenterOffset + ", 20)");
-    svg.attr("height", g.graph().height + 40);
+        inner = svg.append("g");
+    // Set up zoom support
+    var zoom = d3.behavior.zoom().on("zoom", function() {
+      inner.attr("transform", "translate(" + d3.event.translate + ")" +
+                 "scale(" + d3.event.scale + ")");
+    });
+    svg.call(zoom);
+    render(inner, g);
+    inner.selectAll("g.node")
+      .attr("title", function(v) { return g.node(v).fullname })
+      .on("mouseover",function(v) { console.log(g.node(v).log); });
+    // Center the graph
+//    var initialScale = 0.55;
+//    zoom
+//      .translate([(svg.attr("width") - g.graph().width * initialScale) / 2, 20])
+//      .scale(initialScale)
+//      .event(svg);
+//    svg.attr('height', g.graph().height * initialScale + 40);
   }
 
   render() {
     return (
+      <div>
+      <h4>Version DAG</h4>
       <div className="dag"><svg width="100%"><g/></svg></div>
+      </div>
     );
   }
 }
