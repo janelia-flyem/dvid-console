853a607db8dcf1cef1ceb5e60eacbcefd9d28419
Jody Clements <jody@neomorphic.co.uk>
2015-07-20 17:24:26 -0400

    Stops wrong data from being displayed temporarily.
    If there isn't a check to make sure that the uuid matches the repo meta
    information, then the page can display the old data from the previous
    repo before the current one is loaded. This causes a flash of old
    content to be displayed and is not good, especially if on a slow
    connection.

--- js/components/RepoMeta.react.js
+++ js/components/RepoMeta.react.js
@@ -6,20 +6,40 @@ class RepoMeta extends React.Component {
   render () {
     var repo = this.props.repo;
 
-    return (
-      <div className="repometa row">
-        <div className="col-sm-6">
-          <h3>{repo.Alias || "<Nameless Repo>"}</h3>
-          <p>{repo.Description}</p>
-          <p><b>UUID:</b> <Link to="repo" params={{uuid: this.props.uuid}}>{this.props.uuid}</Link></p>
+    if (repo.DAG.Nodes.hasOwnProperty(this.props.uuid)) {
+      return (
+        <div className="repometa row">
+          <div className="col-sm-6">
+            <h3>{repo.Alias || "<Nameless Repo>"}</h3>
+            <p>{repo.Description}</p>
+            <p><b>UUID:</b> <Link to="repo" params={{uuid: this.props.uuid}}>{this.props.uuid}</Link></p>
+          </div>
+          <div className="col-sm-6 text-right">
+            <p><b>Root UUID:</b> <Link to="repo" params={{uuid: repo.Root}}>{repo.Root}</Link></p>
+            <p><b>Created:</b> {moment(repo.Created).format("MMM Do YYYY, h:mm:ss a")}</p>
+            <p><b>Updated:</b> {moment(repo.Updated).format("MMM Do YYYY, h:mm:ss a")}</p>
+          </div>
         </div>
-        <div className="col-sm-6 text-right">
-          <p><b>Root UUID:</b> <Link to="repo" params={{uuid: repo.Root}}>{repo.Root}</Link></p>
-          <p><b>Created:</b> {moment(repo.Created).format("MMM Do YYYY, h:mm:ss a")}</p>
-          <p><b>Updated:</b> {moment(repo.Updated).format("MMM Do YYYY, h:mm:ss a")}</p>
+      );
+
+    }
+    else {
+      return (
+        <div className="repometa row">
+          <div className="col-sm-6">
+            <h3>Loading...</h3>
+            <p>...</p>
+            <p><b>UUID:</b> </p>
+          </div>
+          <div className="col-sm-6 text-right">
+            <p><b>Root UUID:</b></p>
+            <p><b>Created:</b> </p>
+            <p><b>Updated:</b> </p>
+          </div>
         </div>
-      </div>
-    );
+      );
+
+    }
   }
 }
 
