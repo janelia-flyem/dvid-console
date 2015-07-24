f62fb17127b357617d919290f153dc8d04850a00
Jody Clements <jody@neomorphic.co.uk>
2015-07-24 11:11:22 -0400

    Removed link that went nowhere.

--- js/components/RepoMeta.react.js
+++ js/components/RepoMeta.react.js
@@ -12,7 +12,7 @@ class RepoMeta extends React.Component {
           <div className="col-sm-6">
             <h3>{repo.Alias || "<Nameless Repo>"}</h3>
             <p>{repo.Description}</p>
-            <p><b>UUID:</b> <Link to="repo" params={{uuid: this.props.uuid}}>{this.props.uuid}</Link></p>
+            <p><b>UUID:</b> {this.props.uuid}</p>
           </div>
           <div className="col-sm-6 text-right">
             <p><b>Root UUID:</b> <Link to="repo" params={{uuid: repo.Root}}>{repo.Root}</Link></p>
