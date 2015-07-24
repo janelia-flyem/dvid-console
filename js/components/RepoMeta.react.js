d8ed3126d60cf5e80843391b3e9cbf4b67801159
Jody Clements <jody@neomorphic.co.uk>
2015-07-24 13:19:53 -0400

    Adds a link to the repo information in JSON format
    This restores functionality that was in the php DVID dashboard.
    Fixes #7

--- js/components/RepoMeta.react.js
+++ js/components/RepoMeta.react.js
@@ -6,13 +6,15 @@ class RepoMeta extends React.Component {
   render () {
     var repo = this.props.repo;
 
+    var dataLink = "/api/repo/" + this.props.uuid + "/info";
+
     if (repo.DAG.Nodes.hasOwnProperty(this.props.uuid)) {
       return (
         <div className="repometa row">
           <div className="col-sm-6">
             <h3>{repo.Alias || "<Nameless Repo>"}</h3>
             <p>{repo.Description}</p>
-            <p><b>UUID:</b> {this.props.uuid}</p>
+            <p><b>UUID:</b><a href={dataLink}> {this.props.uuid}</a></p>
           </div>
           <div className="col-sm-6 text-right">
             <p><b>Root UUID:</b> <Link to="repo" params={{uuid: repo.Root}}>{repo.Root}</Link></p>
