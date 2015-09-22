19034f8c4c892c408c7bcd009aa849b939f0a1fd
Jody Clements <jody@neomorphic.co.uk>
2015-09-22 09:39:52 -0400

    Cleaned up the coordinate controls
    Added bootstrap styling and reduced the size of the input elements.

--- js/components/TileMapArea.react.js
+++ js/components/TileMapArea.react.js
@@ -596,19 +596,19 @@ var TileMapArea = React.createClass({
     // need to figure out what plane we are in and change the XYZ labels accordingly.
     //
 
-    var inputOne   = React.createElement('input',{'id': 'horizontal', 'type': 'number', 'min': 0, 'ref': 'horizontal'});
-    var inputTwo   = React.createElement('input',{'id': 'vertical', 'type': 'number', 'min': 0, 'ref': 'vertical'});
-    var inputThree = React.createElement('input',{'id': 'depth', 'type': 'number', 'min': 0, 'ref': 'depth'});
+    var inputOne   = React.createElement('input',{'id': 'horizontal', 'type': 'number', 'min': 0, 'ref': 'horizontal', 'className': 'form-control input-sm'});
+    var inputTwo   = React.createElement('input',{'id': 'vertical', 'type': 'number', 'min': 0, 'ref': 'vertical', 'className': 'form-control input-sm'});
+    var inputThree = React.createElement('input',{'id': 'depth', 'type': 'number', 'min': 0, 'ref': 'depth', 'className': 'form-control input-sm'});
 
     if (this.state.plane === 1) {
-      inputOne   = React.createElement('input',{'id': 'horizontal', 'type': 'number', 'min': 0, 'ref': 'horizontal'});
-      inputTwo   = React.createElement('input',{'id': 'depth', 'type': 'number', 'min': 0, 'ref': 'depth'});
-      inputThree = React.createElement('input',{'id': 'vertical', 'type': 'number', 'min': 0, 'ref': 'vertical'});
+      inputOne   = React.createElement('input',{'id': 'horizontal', 'type': 'number', 'min': 0, 'ref': 'horizontal', 'className': 'form-control input-sm'});
+      inputTwo   = React.createElement('input',{'id': 'depth', 'type': 'number', 'min': 0, 'ref': 'depth', 'className': 'form-control input-sm'});
+      inputThree = React.createElement('input',{'id': 'vertical', 'type': 'number', 'min': 0, 'ref': 'vertical', 'className': 'form-control input-sm'});
     }
     else if (this.state.plane === 2) {
-      inputOne   = React.createElement('input',{'id': 'depth', 'type': 'number', 'min': 0, 'ref': 'depth'});
-      inputTwo   = React.createElement('input',{'id': 'horizontal', 'type': 'number', 'min': 0, 'ref': 'horizontal'});
-      inputThree = React.createElement('input',{'id': 'vertical', 'type': 'number', 'min': 0, 'ref': 'vertical'});
+      inputOne   = React.createElement('input',{'id': 'depth', 'type': 'number', 'min': 0, 'ref': 'depth', 'className': 'form-control input-sm'});
+      inputTwo   = React.createElement('input',{'id': 'horizontal', 'type': 'number', 'min': 0, 'ref': 'horizontal', 'className': 'form-control input-sm'});
+      inputThree = React.createElement('input',{'id': 'vertical', 'type': 'number', 'min': 0, 'ref': 'vertical', 'className': 'form-control input-sm'});
     }
 
     var segmentation_active = this.state.segmentation ? 'active' : '';
@@ -654,11 +654,17 @@ var TileMapArea = React.createClass({
         <div id="viewer" className="openseadragon"></div>
         <div className="row">
           <div className="col-sm-12">
-            <form name="coordinates" onSubmit={this.handleCoordinateChange}>
-              <label>x</label>{inputOne}
-              <label>y</label>{inputTwo}
-              <label>z</label>{inputThree}
-              <button type="submit" id="coordinatechange">Go</button>
+            <form className="form-inline" name="coordinates" onSubmit={this.handleCoordinateChange} id="coordinates">
+              <div className="form-group">
+                <label>x</label>{inputOne}
+              </div>
+              <div className="form-group">
+                <label>y</label>{inputTwo}
+              </div>
+              <div className="form-group">
+                <label>z</label>{inputThree}
+              </div>
+              <button  className="btn btn-default btn-sm" type="submit" id="coordinatechange">Go</button>
             </form>
           </div>
         </div>
