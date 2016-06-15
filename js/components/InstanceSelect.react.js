d95a947fa69eb93e78029effd3afa1b60187cb43
Jody Clements <jody@neomorphic.co.uk>
2016-06-15 16:00:03 -0400

    Adds button on each repo page to open neuroglancer
    This makes it possible to open neuroglancer with any combination of uuid
    and data source available in a single repo.

--- js/components/InstanceSelect.react.js
+++ js/components/InstanceSelect.react.js
@@ -51,6 +51,29 @@ var InstanceSelect = React.createClass({
     }
   },
 
+  neuroGlancerHandler: function(event) {
+    var tile_source = null,
+      label_source = null;
+    // grab tile source selection
+    tile_source = $("#instance_select input:radio[name=tile_source]:checked").val();
+    // grab label source selection
+    label_source = $("#instance_select input:radio[name=label_source]:checked").val();
+    // display an error message if either one is missing.
+    if(!tile_source) {
+      ErrorActions.update('Please select a tile source from the table below.');
+      return;
+    }
+
+    ErrorActions.clear();
+    // generate a new url with the choices made and ...
+    // redirect the browser
+    var port = window.location.port || "80"
+    var host_string = window.location.hostname + ":" + port;
+    var glancer_url = "/neuroglancer/#!{%27layers%27:{%27" + tile_source + "%27:{%27type%27:%27image%27_%27source%27:%27dvid://http://" + host_string + "/"+ this.props.uuid + "/" + tile_source + "%27}}}"
+
+    window.location.href = glancer_url
+  },
+
   render: function () {
     return (
       <div className="dataselect">
@@ -60,6 +83,7 @@ var InstanceSelect = React.createClass({
           <div className="col-sm-3">
             <p>Select a tile source and a label source, then open the tile viewer to preview your data.</p>
             <button className="btn btn-default" onClick={this.showDataHandler}>Open Tile Viewer</button>
+            <button className="btn btn-default" onClick={this.neuroGlancerHandler}>Open Neuroglancer</button>
           </div>
           <div className="col-sm-9">
             <form id="instance_select">
