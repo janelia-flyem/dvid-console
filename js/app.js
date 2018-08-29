aa52e913c69c160f8b283ea1d258e0d26806f574
Jody Clements <jody@neomorphic.co.uk>
2018-08-29 13:58:08 -0400

    Removed old code for rewrite
    Roll on version 3

--- js/app.js
+++
@@ -1,8 +0,0 @@
-require('bootstrap');
-var React = require('react');
-var ConsoleApp = require('./components/ConsoleApp.react');
-
-// turn off global 'lite' switch (har har) to handle conditional 'lite' code
-// makes it possible to 'switch' before the store is instantiated
-window.DVID_LITE = false;
-
