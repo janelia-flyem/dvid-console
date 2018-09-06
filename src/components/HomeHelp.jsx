ea7b042b02fca5909de7a0345d8f00861952f8e3
Jody Clements <jody@neomorphic.co.uk>
2018-09-06 09:07:31 -0400

    Added contents to the help section on the homepage

--- src/components/HomeHelp.jsx
+++ src/components/HomeHelp.jsx
@@ -1,9 +1,47 @@
 import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import SyntaxHighlighter from 'react-syntax-highlighter/prism';
+import { darcula } from 'react-syntax-highlighter/styles/prism';
+
+const DataSourceComponent = () => {
+  const codeString = 'gs://flyem-public-connectome';
+  return <SyntaxHighlighter language="python" style={darcula}>{codeString}</SyntaxHighlighter>;
+};
+
+const ImportComponent = () => {
+  const codeString = [
+    'from diced import DicedStore',
+    'store = DicedStore("<data source>")'].join('\n');
+  return <SyntaxHighlighter language="python" style={darcula}>{codeString}</SyntaxHighlighter>;
+};
+
+const FetchComponent = () => {
+  const codeString = [
+    '# open repo',
+    'repo = store.open_repo("<reponame>")',
+    '# get array',
+    'my_array = repo.get_array("<array_name>")'].join('\n');
+  return <SyntaxHighlighter language="python" style={darcula}>{codeString}</SyntaxHighlighter>;
+};
 
 class HomeHelp extends React.Component {
   render() {
     return (
-      <p>Homepage help card goes here.</p>
+      <Card>
+        <CardContent>
+          <p>Data Source</p>
+          <DataSourceComponent />
+          <p>Get Data from DICED</p>
+          <ol>
+            <li><a href="https://github.com/janelia-flyem/diced/blob/master/README.md#installation">install DICED</a></li>
+            <li><p>Connect to a DICED Store from python</p></li>
+            <ImportComponent />
+            <li><p>Get the data</p></li>
+            <FetchComponent />
+          </ol>
+        </CardContent>
+      </Card>
     );
   }
 }
