698cccd843331aa1496dcf5a455b511abdc338a7
Jody Clements <jody@neomorphic.co.uk>
2022-07-21 11:05:27 -0400

    Adds missing stats to ServerStats component

--- src/Admin.js
+++ src/Admin.js
@@ -1,12 +1,26 @@
 import Typography from "@mui/material/Typography";
+import { useQuery } from "react-query";
+import { getRepos } from "./lib/dvid";
 import ServerStats from "./ServerStats";
 import ServerTypes from "./ServerTypes";
+import ServerStatus from "./ServerStatus";
 
 export default function Admin() {
+  const { isLoading, isError, data, error } = useQuery("repoList", getRepos);
+  if (isLoading) {
+    return <p>Loading</p>;
+  }
+
+  if (isError) {
+    return <p>Error Loading: {error}</p>;
+  }
+
+
   return (
     <div style={{ padding: "1em" }}>
       <Typography variant="h4">Server Stats</Typography>
-      <ServerStats/>
+      <ServerStats repos={data}/>
+      <ServerStatus/>
       <ServerTypes/>
     </div>
   );
--- src/ServerStats.js
+++ src/ServerStats.js
@@ -1,11 +1,11 @@
 import { useQuery } from "react-query";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGear } from "@fortawesome/free-solid-svg-icons";
-import Grid from '@mui/material/Grid';
+import Grid from "@mui/material/Grid";
 import StatsCard from "./StatsCard";
 import { serverInfo } from "./lib/dvid";
 
-export default function ServerStats() {
+export default function ServerStats({ repos }) {
   const { isLoading, isError, data, error } = useQuery(
     "serverInfo",
     serverInfo
@@ -21,6 +21,20 @@ export default function ServerStats() {
 
   const gitLink = "https://github.com/janelia-flyem/dvid/";
 
+  let serverUptime = 0;
+  if (data["Server uptime"]) {
+    serverUptime = data["Server uptime"].split(".", 1);
+  }
+
+  let versionNodes = 0;
+
+  Object.keys(repos).forEach((key) => {
+    const repo = repos[key];
+    if (repo && repo.DAG) {
+      versionNodes += Object.keys(repo.DAG.Nodes).length;
+    }
+  });
+
   const stats = [
     {
       name: "DVID CPU CORES",
@@ -29,25 +43,23 @@ export default function ServerStats() {
     },
     {
       name: "Repositories",
-      value: "unknown",
+      value: Object.keys(repos).length,
       icon: <FontAwesomeIcon icon={faGear} size="xl" />,
     },
     {
       name: "Server Uptime",
-      value: "",
+      value: `${serverUptime}s`,
       icon: <FontAwesomeIcon icon={faGear} size="xl" />,
     },
     {
       name: "Version Nodes",
-      value: "",
+      value: versionNodes,
       icon: <FontAwesomeIcon icon={faGear} size="xl" />,
     },
     {
       name: "DVID Version",
       value: (
-        <a
-        href={`${gitLink}/releases/tag/${data["DVID Version"]}`}
-        >
+        <a href={`${gitLink}/releases/tag/${data["DVID Version"]}`}>
           {data["DVID Version"] || "unknown"}
         </a>
       ),
