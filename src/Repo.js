f48959412d98b41b66788fc4c870d394b6366790
Jody Clements <jody@neomorphic.co.uk>
2022-07-25 09:24:52 -0400

    Restores basic RepoLog functionality

--- src/Repo.js
+++ src/Repo.js
@@ -1,6 +1,50 @@
 import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useQuery } from "react-query";
+import Grid from "@mui/material/Grid";
+import { repoInfo } from "./lib/dvid";
+import RepoLog from "./RepoLog";
+
+function getFullUUIDinRepo(repo, shortuuid) {
+  if (repo === null) {
+    return shortuuid;
+  }
+  for (var id in repo.DAG.Nodes) {
+    if (RegExp("^" + shortuuid).test(id)) {
+      //no need to update repo--uuid is in current repo
+      return id;
+    }
+  }
+  return shortuuid;
+}
 
 export default function Repo() {
   const { repoId } = useParams();
-  return <p>Repo: {repoId}</p>;
+  const [fullRepoId, setFullRepoId] = useState(repoId);
+  const { isLoading, isError, data, error } = useQuery(
+    ["repoInfo", repoId],
+    () => repoInfo({ uuid: repoId, endpoint: "info" })
+  );
+
+  useEffect(() => {
+    if (data) {
+      setFullRepoId(getFullUUIDinRepo(data, repoId));
+    }
+  }, [repoId, data]);
+
+  if (isLoading) {
+    return <p>Loading</p>;
+  }
+
+  if (isError) {
+    return <p>Error Loading: {error.message}</p>;
+  }
+
+  return (
+    <div style={{ padding: "1em" }}>
+      <Grid container spacing={2}>
+        <RepoLog log={data.Log} uuid={fullRepoId} />
+      </Grid>
+    </div>
+  );
 }
---
+++ src/RepoLog.js
@@ -0,0 +1,47 @@
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardContent from "@mui/material/CardContent";
+import Grid from "@mui/material/Grid";
+import { format, formatDistance } from "date-fns";
+
+import "./RepoLog.css";
+
+export default function RepoLog({ log, uuid }) {
+
+  const logDuration = formatDistance(new Date(log[0].split(/\s(.+)/)[0]), new Date(log[log.length -1].split(/\s(.+)/)[0]));
+
+
+  return (
+    <Grid item xs={12}>
+      <Card className="repoLog">
+        <CardHeader
+          title="Repo Log:"
+          titleTypographyProps={{ variant: "p" }}
+          sx={{ background: "#eee" }}
+        action={<><span>{log.length} entries covering {logDuration} </span></>}
+        />
+        <CardContent>
+          <table>
+            <tbody>
+              {log
+                .slice()
+                .reverse()
+                .map((entry, i) => {
+                  const [date, description] = entry.split(/\s(.+)/);
+                  return (
+                    <tr key={`${date}:${i}`} className={i % 2 ? "even" : "odd"}>
+                      <td style={{ width: "14em" }}>
+                        {" "}
+                        {format(new Date(date), "MMM do yyyy, h:mm:ss a")}
+                      </td>
+                      <td>{description}</td>
+                    </tr>
+                  );
+                })}
+            </tbody>
+          </table>
+        </CardContent>
+      </Card>
+    </Grid>
+  );
+}
