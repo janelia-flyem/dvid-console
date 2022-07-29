586ec6c89abc3f7fc7f8f9087a7834c0f8a6fbbb
Jody Clements <jody@neomorphic.co.uk>
2022-07-29 17:17:54 -0400

    Restores all the DAG action buttons.
    These are the buttons that zoom onto specific nodes or download the SVG
    for use elsewhere.

--- src/RepoDAG.js
+++ src/RepoDAG.js
@@ -1,11 +1,25 @@
 import { useEffect, useRef } from "react";
 import Grid from "@mui/material/Grid";
 import Card from "@mui/material/Card";
+import IconButton from "@mui/material/IconButton";
+import Tooltip from "@mui/material/Tooltip";
 import Typography from "@mui/material/Typography";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faLocationCrosshairs,
+  faMaximize,
+  faDownload,
+} from "@fortawesome/free-solid-svg-icons";
 import dagreD3 from "dagre-d3";
 import * as d3 from "d3";
 
 import "./RepoDAG.css";
+
+const masterFill = "#8bc34a";
+const masterStroke = "#689732";
+
+const currentFill = "#fcf8e3";
+const currentStroke = "#efd968";
 
 // get the actual node with all information from the original DAG
 function getNodeByVersion(dag, id) {
@@ -27,6 +41,7 @@ function createNode(node, dagGraph, uuid, masterUUID) {
   const version = node.VersionID;
   const log = node.Log.length ? node.Log : [];
   let nodeclass = "";
+  let nodeStyle = "fill: #fff; stroke: #333; stroke-width: 1.5px";
 
   if (node.Locked) {
     nodeclass += "type-locked";
@@ -38,6 +53,7 @@ function createNode(node, dagGraph, uuid, masterUUID) {
 
   if (masterUUID && RegExp("^" + masterUUID).test(node.UUID)) {
     nodeclass = `${nodeclass} master`;
+    nodeStyle = `fill: ${masterFill}; stroke: ${masterStroke}; stroke-width: 1.5px`;
   }
 
   /* else if (props.repoMasterBranchHist) {
@@ -52,15 +68,17 @@ function createNode(node, dagGraph, uuid, masterUUID) {
   */
   if (node.UUID === uuid) {
     nodeclass = `${nodeclass} current`;
+    nodeStyle = `fill: ${currentFill}; stroke: ${currentStroke}; stroke-width: 1.5px`;
   }
 
   dagGraph.setNode(version, {
-    label: version + ": " + node.UUID.substr(0, 5),
+    label: `${version}: ${node.UUID.substr(0, 5)} ${node.Locked ? "L" : "O"}`,
     class: nodeclass,
     rx: 5,
     ry: 5,
     log: log,
     note: note,
+    style: nodeStyle,
     fullname: version + ": " + node.UUID,
     uuid: node.UUID,
     id: "node" + version,
@@ -73,7 +91,10 @@ function createNode(node, dagGraph, uuid, masterUUID) {
 
 function createEdges(node, dagGraph) {
   node.Children.forEach((child) => {
-    dagGraph.setEdge(node.VersionID, child);
+    dagGraph.setEdge(node.VersionID, child, {
+      style:
+        "stroke: #333; stroke-width: 1.5px; stroke-opacity:1; fill:none; fill-opacity:1",
+    });
   });
 }
 
@@ -113,35 +134,62 @@ function attachEvents(nodes, tooltipRef, dag) {
     });
 }
 
-function setZoom(center, scale, transformGroup, container) {
-  const zoom = d3.zoom().on("zoom", (e) => {
-    transformGroup.attr("transform", d3.event.transform);
-  });
-  d3.select(container).call(zoom);
-  // zoom.scale(scale);
-  // zoom.translateTo(transformGroup, center);
+function fitGraphToWindow(container, zoom, dagGraph, svgRef, duration = 600) {
+  // determine container dimensions
+  const svgNode = d3.select(svgRef).node();
+  // calculate scale to fit either height or width
+  const scale = Math.min(
+    svgNode.clientWidth / (dagGraph.graph().width + 100),
+    svgNode.clientHeight / (dagGraph.graph().height + 100)
+  );
+  // only change scale if it is lager than the window. We don't want
+  // to blow up a single node to fill the whole window.
+  const fixedScale = scale > 1 ? 1 : scale;
+
+  // calculate x & y offset to place scaled graph at display center
+  const xOffset = Math.abs(
+    (dagGraph.graph().width * scale - svgNode.clientWidth) / 2
+  );
+  const yOffset = Math.abs(
+    (dagGraph.graph().height * scale - svgNode.clientHeight) / 2
+  );
+
+  d3.select(svgRef)
+    .select("svg")
+    .transition()
+    .duration(duration)
+    .call(
+      zoom.transform,
+      d3.zoomIdentity.translate(xOffset, yOffset).scale(fixedScale)
+    );
 }
 
-function scrollToNode(nodeSelector, transformGroup, container, zoom) {
+function scrollToNode(
+  nodeSelector,
+  transformGroup,
+  container,
+  zoom,
+  duration = 0
+) {
   // figure out the scale ratio that will be used to resize the graph.
   var node = d3.select(nodeSelector);
   if (!node.empty()) {
-    const coords = node.node().getBoundingClientRect();
+    // const coords = node.node().getBoundingClientRect();
+    const [, x, y] = node
+      .attr("transform")
+      .match(/translate\((-?\d+\.?\d+),(-?\d+\.?\d+)/);
     //create a translation that will center this node on the display
     var newT = {
-      x: container.clientWidth / 2 - coords.x,
+      x: container.clientWidth / 2 - x,
       // y: container.clientHeight / 2 - coords.y,
-      y: container.clientHeight / 2 + 500 - coords.y,
+      y: container.clientHeight / 2 - y,
     };
-    d3.select(container).select('svg')
-      .call(zoom.transform, d3.zoomIdentity.translate(newT.x, newT.y).scale(1))
+    d3.select(container)
+      .select("svg")
+      .transition()
+      .duration(duration)
+      .call(zoom.transform, d3.zoomIdentity.translate(newT.x, newT.y).scale(1));
 
-    // transformGroup
-    //  .transition()
-    //  .duration(300)
-    //  .attr("transform", "translate(" + newT.x + ", " + newT.y + ")");
-    // pass scale=1, since we're using the initial element size
-    // setZoom([newT.x, newT.y], 1, transformGroup, container);
     return true;
   } else {
     return false;
@@ -181,13 +229,13 @@ function initGraph(ref, dag, uuid, masterUUID, tooltipRef) {
   // generate the main svg image
   var svg = container
     .append("svg")
+    .attr("id", "dagGraph")
     .attr("width", ref.clientWidth)
     .attr("height", 500);
   // attach a group object for managing the graph.
-  const transformGroup = svg.append("g")
-    .on("mouseenter", (id) => {
-      d3.select(tooltipRef).html("").style("opacity", 0);
-    });
+  const transformGroup = svg.append("g").on("mouseenter", (id) => {
+    d3.select(tooltipRef).html("").style("opacity", 0);
+  });
 
   // & attach pan / zoom functionality
   const zoom = d3.zoom().on("zoom", () => {
@@ -203,25 +251,102 @@ function initGraph(ref, dag, uuid, masterUUID, tooltipRef) {
   // center the DAG on current selected node, master node or most
   // recently created node.
   scrollToNode(".node.current", transformGroup, ref, zoom);
+  return [svg, transformGroup, zoom, g];
 }
 
 export default function RepoDAG({ dag, uuid, masterUUID }) {
   const DAGImage = useRef(null);
   const DAGTooltip = useRef(null);
+  const graphData = useRef();
 
   useEffect(() => {
-    initGraph(DAGImage.current, dag, uuid, masterUUID, DAGTooltip.current);
+    const graph = initGraph(
+      DAGImage.current,
+      dag,
+      uuid,
+      masterUUID,
+      DAGTooltip.current
+    );
+    graphData.current = graph;
   }, [dag, uuid, DAGImage, masterUUID]);
 
-  const handleClick = () => {
-    console.log("handleclick");
+  const handleScrollToMaster = () => {
+    scrollToNode(
+      ".node.master",
+      graphData.current[1],
+      DAGImage.current,
+      graphData.current[2],
+      600
+    );
+  };
+
+  const handleScrollToCurrent = () => {
+    scrollToNode(
+      ".node.current",
+      graphData.current[1],
+      DAGImage.current,
+      graphData.current[2],
+      600
+    );
+  };
+
+  const handleFitGraph = () => {
+    fitGraphToWindow(
+      graphData.current[1],
+      graphData.current[2],
+      graphData.current[3],
+      DAGImage.current
+    );
+  };
+
+  const handleDownloadSVG = () => {
+    var svgData = document.getElementById("dagGraph").outerHTML;
+    var svgBlob = new Blob([svgData], { type: "image/svg+xml;charset=utf-8" });
+    var svgUrl = URL.createObjectURL(svgBlob);
+    var downloadLink = document.createElement("a");
+    downloadLink.href = svgUrl;
+    downloadLink.download = `${uuid}.svg`;
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
   };
 
   return (
     <Grid item xs={12}>
-      <button onClick={handleClick}>ScrollTo</button>
       <Typography variant="h5">Version History</Typography>
-      <Card variant="outlined" ref={DAGImage}></Card>
+      <Card variant="outlined">
+        <div className="DAGControls">
+          <Tooltip title="Scroll to master node">
+            <IconButton onClick={handleScrollToMaster}>
+              <FontAwesomeIcon
+                icon={faLocationCrosshairs}
+                size="sm"
+                style={{ color: masterFill }}
+              />
+            </IconButton>
+          </Tooltip>
+          <Tooltip title="Scroll to current node">
+            <IconButton onClick={handleScrollToCurrent}>
+              <FontAwesomeIcon
+                icon={faLocationCrosshairs}
+                size="sm"
+                style={{ color: currentStroke }}
+              />
+            </IconButton>
+          </Tooltip>
+          <Tooltip title="Fit graph to window">
+            <IconButton onClick={handleFitGraph}>
+              <FontAwesomeIcon icon={faMaximize} size="sm" />
+            </IconButton>
+          </Tooltip>
+          <Tooltip title="Download Graph">
+            <IconButton onClick={handleDownloadSVG}>
+              <FontAwesomeIcon icon={faDownload} size="sm" />
+            </IconButton>
+          </Tooltip>
+        </div>
+        <div ref={DAGImage}></div>
+      </Card>
       <div ref={DAGTooltip} className="dagTooltip"></div>
     </Grid>
   );
